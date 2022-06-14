97e874c8ae41a91a3bb85b4ac0261af69db996d4
ThierryMatu <101680040+ThierryMatu@users.noreply.github.com>
2022-06-15 00:36:45 +0400

    Add files via upload

--- test.js
+++ test.js
@@ -4,11 +4,23 @@ const path = require("path")
 const fs = require("fs")
 const http = require("http")
 const app = express()
-const port = process.env.PORT || 4400
+
+// const bodyParser = require('body-parser')
+
+// CORS allows you to configure the web API's security. 
+// It has to do with allowing other domains to make requests against your web API.
+app.use(cors());
 
 // MIDDLEWARES
+// Create an Express.js instance:
 // use Parser Middleware
 app.use(express.json())
+app.set('port', 3000)
+app.use ((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+})
 
 // use Logger middleware
 app.use(function (req, res, next) {
@@ -24,84 +36,94 @@ app.param('collectionName', function (req, res, next, collectionName) {
 })
 
 // use static file middleware
-app.use(function (req, res, next) {
-  const filePath = path.join(__dirname, 'static', req.url)
-  fs.stat(filePath, function (err, fileInfo) {
-    if (err) return next()
-    if (fileInfo.isFile()) res.sendFile(filePath)
-    else next()
+
+app.use(function(req, res, next){
+  var filePath = path.join(__dirname, "static", req.url)
+  fs.stat(filePath, function(err, fileInfo){
+      if (err) {
+          next()
+          return
+      }
+      if (fileInfo.isFile()) {
+          res.sendFile(filePath)
+      }
+      else{
+          next()
+      }
   })
 })
 
-// CORS allows you to configure the web API's security. 
-// It has to do with allowing other domains to make requests against your web API.
-app.use(cors());
 
-const { MongoClient, ObjectId } = require('mongodb')
-const username = 'Enoch'
-const password = 'Matu'
-const dbName = 'afterschool'
-let db = null
 
-// Connect to MongoDB database
-async function main () {
-  const client = new MongoClient(`mongodb+srv://${username}:${password}@cluster0.5g703.mongodb.net/${dbName}?retryWrites=true&w=majority`)
-  try {
-    await client.connect()
-    db = client.db(dbName)
-    console.log('connected')
-  }
-  catch(err) {
-    console.error('ERR', err)
-  }
-}
-main().catch(console.error)
+// connect to MongoDB Database
+const MongoClient = require('mongodb').MongoClient;
+let db;
+MongoClient.connect('mongodb+srv://Enoch:Matu@cluster0.4b3r7.mongodb.net/test', (err, client) => {
+    db = client.db('afterschool')
+})
 
-// Endpoint to get user (from group CW)
-app.get('/user', async function (req, res, next) {
-  res.json({ email: 'user@email.com', password: 'mypassword' })
+
+
+// retrieve all the objects from an collection
+app.get('/collection/:collectionName', (req, res, next) => {
+  req.collection.find({}).toArray((e, results) => {
+      if (e) return next(e)
+      res.send(results)
+  })
+})
+
+
+//adding post to the collection or uploading a post to the collection
+//New value or object to be inserted to the collection
+app.post('/collection/:collectionName', (req, res, next) => {
+  req.collection.insert(req.body, (e, results) => {
   if (e) return next(e)
-});
-
-// Endpoint to get all lessons
-app.get('/collection/:collectionName', async function (req, res, next) {
-  await req.collection.find().toArray((e, results) => {
-    if (e) return next(e)
-    res.json(results)
+  res.send(results.ops)
   })
-});
-
-// Endpoint to add an order
-app.post('/collection/:collectionName', async function (req, res, next) {
-  await req.collection.insert(req.body, (e, results) => {
-    if (e) return next(e)
-    res.status(201).send(results)
   })
-})
+  
+
 
 // Endpoint to update number of available spaces in lesson
-app.put('/collection/:collectionName/:id', async function (req, res, next) {
-  await req.collection.updateOne(
-    { _id: new ObjectId(req.params.id) },
-    { $set: { space: req.body.space } },
-    (e, results) => {
-      res.json(results)
-      if (e) return next(e)
+//update an object 
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+  req.collection.updateOne(
+  {_id: new ObjectID(req.params.id)},
+  {$set: req.body},
+  {safe: true, multi: false},
+  (e, result) => {
+  if (e) return next(e)
+  res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
   })
-})
+  })
+  
 
 // Endpoint to perform a Full Text Search on lessons
-app.get('/search/:collectionName', async function (req, res, next) {
-  const topic = new RegExp(`.*${req.query.query}.*`, 'gi')
-  await req.collection.find({ topic: topic })
-  .toArray((err, results) => {
-    res.json(results)
-    if (err) return next(err)
+app.get('/collection/:collectionName/search', (req, res, next) => {
+  let search_keyword = req.query.search
+  req.collection.find({})
+  .toArray((e, results) => {
+    if (e) return next(e)
+    let filteredList = results.filter((lesson) => {
+      return lesson.title.toLowerCase().match(search_keyword.toLowerCase()) || lesson.location.toLowerCase().match(search_keyword.toLowerCase())
+  });  
+  res.send(filteredList)
+
   })
 })
 
+app.get('/', (req, res) => {
+  res.send("Welcome to entry point")
+})
+
+
 // Listen to port
-app.listen(port, function () {
-  console.log(`Server running at http://localhost:${port}`);
-});
+
+const port = process.env.PORT || 3000
+
+app.listen(3000, () => {
+  console.log('Express.js server running at localhost:3000')
+})
+
+
 
